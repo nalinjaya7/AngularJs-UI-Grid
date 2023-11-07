1f15d6fbb57f2666accbecc45dca0a878a99dcbf
Nalin Jayawardana <116587602+nalinjaya7@users.noreply.github.com>
2023-11-07 15:06:20 +0530

    Some Changes in homecontroller code  Update README.md

--- AngularJsUI-Grid/Controllers/HomeController.cs
+++ AngularJsUI-Grid/Controllers/HomeController.cs
@@ -36,14 +36,26 @@ namespace AngularJsUI_Grid.Controllers
             models.Add(new OrderModel("Ord011", "Prasanna", 1380.00m, DateTime.Now.AddDays(-65), OrderStatus.Order, 2.00m));
             models.Add(new OrderModel("Ord010", "Nalin", 677.00m, DateTime.Now.AddDays(-66), OrderStatus.SalesOrder, 8.00m));
             models.Add(new OrderModel("Ord009", "Thilina", 712.00m, DateTime.Now.AddDays(-68), OrderStatus.SalesOrder, 0.00m));
-            models.Add(new OrderModel("Ord008","Anil",130.00m,DateTime.Now.AddDays(-69), OrderStatus.Order,2.00m));
+            models.Add(new OrderModel("Ord008", "Anil", 130.00m, DateTime.Now.AddDays(-69), OrderStatus.Order, 2.00m));
             models.Add(new OrderModel("Ord007", "Rohith", 677.00m, DateTime.Now.AddDays(-69), OrderStatus.SalesOrder, 8.00m));
             models.Add(new OrderModel("Ord006", "Ajith", 12.00m, DateTime.Now.AddDays(-70), OrderStatus.SalesOrder, 0.00m));
-            models.Add(new OrderModel("Ord005","Jhone",6740.00m, DateTime.Now.AddDays(-71), OrderStatus.Order,10.00m));
-            models.Add(new OrderModel("Ord004","Lalith",98780.00m, DateTime.Now.AddDays(-72), OrderStatus.Cancelled,12.00m));
+            models.Add(new OrderModel("Ord005", "Jhone", 6740.00m, DateTime.Now.AddDays(-71), OrderStatus.Order, 10.00m));
+            models.Add(new OrderModel("Ord004", "Lalith", 98780.00m, DateTime.Now.AddDays(-72), OrderStatus.Cancelled, 12.00m));
             models.Add(new OrderModel("Ord003", "Parera", 321.00m, DateTime.Now.AddDays(-74), OrderStatus.Order, 2.00m));
-            models.Add(new OrderModel("Ord002","Kumar",876.00m, DateTime.Now.AddDays(-75), OrderStatus.Cancelled,41.00m));
+            models.Add(new OrderModel("Ord002", "Kumar", 876.00m, DateTime.Now.AddDays(-75), OrderStatus.Cancelled, 41.00m));
             models.Add(new OrderModel("Ord001", "Gihan", 98767.00m, DateTime.Now.AddDays(-76), OrderStatus.SalesOrder, 200.00m));
+
+
+            var options = Enum.GetValues<OrderStatus>().Where(h => h.ToString() != "All").ToList().Select(g => new { value = (g).ToString(), label = g.ToString() });
+            var serializer = new Newtonsoft.Json.JsonSerializer();
+            var stringWriter = new System.IO.StringWriter();
+            using (var writer = new Newtonsoft.Json.JsonTextWriter(stringWriter))
+            {
+                writer.QuoteChar = '\'';   //replace double quotation using single quotation
+                writer.QuoteName = false;
+                serializer.Serialize(writer, options);
+            }
+            ViewData["OrderStatusjson"] = stringWriter.ToString();
             return View(models);
         }
 
